24ded7ec8271174411d16e38dc023fb07e096fab
Radhikabairi <radhikareddy300@gmail.com>
2019-03-26 14:47:53 +0530

    second commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase :{
+    apiKey: "AIzaSyA68o5FRmxJP3PDIYK8JG2olY3BAJa1Pj4",
+    authDomain: "smarttable-7dd11.firebaseapp.com",
+    databaseURL: "https://smarttable-7dd11.firebaseio.com",
+    projectId: "smarttable-7dd11",
+    storageBucket: "smarttable-7dd11.appspot.com",
+    messagingSenderId: "363829662427"
+  }
+};
